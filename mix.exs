83cf43fd15dad0d680d530012dc1cc6d2a9a8e07
Colin Rymer <colin.rymer@gmail.com>
2016-03-25 14:25:37 -0400

    add ex_doc and bump to v1.0.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Leftpad.Mixfile do
 
   def project do
     [app: :leftpad,
-     version: "1.0.0",
+     version: "1.0.1",
      elixir: "~> 1.2",
      description: "left pad for elixir, because why not? ¯\\_(ツ)_/¯",
      package: package,
@@ -15,7 +15,7 @@ defmodule Leftpad.Mixfile do
   end
 
   defp deps do
-    []
+    [{:ex_doc, ">= 0.11.4", only: [:dev]}]
   end
 
   defp package do
